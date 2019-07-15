41bd5a5f92b9bf6c9164e5738f9b07d4a56ee547
ardulogic <denncath@gmail.com>
2019-07-15 11:03:03 +0300

    Removed duplicated files

--- public_html/index.php
+++ public_html/index.php
@@ -1,35 +1,8 @@
-<?php
-// Uzkraunam visus reikalingus failus
-require '../config.php';
+<h2>Įvestas vardas:</h2>
+<p><?php print $_POST['name'] ?? '-'; ?></p>
 
-
-?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Welcome To PHP FightClub!</title>
-        <link rel="stylesheet" href="media/css/normalize.css">
-        <link rel="stylesheet" href="media/css/style.css">
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">        
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
-        <script src="media/js/app.js"></script>    
-    </head>
-    <body>
-        <!-- $nav Navigation generator -->
-        <?php require ROOT . '/templates/navigation.tpl.php'; ?>        
-        <h1>
-            Welcome to the GAME!!!
-        </h1>
-        <?php if (isset($message)): ?>
-            <div class="message">
-                <span class="text"><?php print $message; ?></span>
-                <span class="close">X</span>
-            </div>
-        <?php endif; ?>
-
-        <!-- $form HTML generator -->
-        <?php require ROOT . '/templates/form.tpl.php'; ?>
-    </body>
-</html>
+<h1>Įveskite vardą</h1>
+<form method="POST">
+    <input name="name"  type="text" placeholder="title">
+    <button name="action" value="save">Save</button>
+</form>
